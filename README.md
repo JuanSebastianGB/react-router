09dadd57a491f7fe0bda6cb242a29859af0d3432
JuanSebastianGB <3393@holbertonschool.com>
2021-12-20 02:22:48 +0000

    UPD: Saving not stored changes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Doc source
 
-[Check out the completed version of the app here](https://stackblitz.com/edit/github-agqlf5?file=src/App.jsx). :eye:
+[Check out the completed version of the app here](https://reactrouter.com/docs/en/v6/getting-started/tutorial). :eye:
 
 ### Installing react
 
@@ -17,3 +17,14 @@ For this project all the files listed inside are deleted
 ### Installing react router
 
     npm i react-router-dom
+
+### To avoid errors
+
+    Is necesary to delete and reinstall node modules, otherwise probably you will get an error
+```bash
+
+rm -rf node_modules
+npm i
+
+```
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import UserPage from './pages/UserPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={
-          <div>Hellow World</div>
-        }></Route>
+        <Route path='/' element={<HomePage></HomePage>}></Route>
+        <Route path='/about' element={<AboutPage></AboutPage>}></Route>
+        <Route path='/users' element={<UserPage></UserPage>}></Route>
+        <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
       </Routes>
     </Router>
   );
