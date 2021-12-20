0e408c127431b23659d860eb2ce63e70bae8a06a
JuanSebastianGB <3393@holbertonschool.com>
2021-12-20 02:58:45 +0000

    UPD: Creating link tag

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom';
+const userId = 20;
 export default function HomePage () {
-  return <div>Hellow World</div>;
+  return (
+    <div>
+      <h1>Application</h1>
+      <Link to={`/users/${userId}`}>Usuarios</Link>
+    </div>
+  );
 }
