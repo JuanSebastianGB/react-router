603c710cb5bbf023a0cb96e6080fc9264edd27b0
JuanSebastianGB <3393@holbertonschool.com>
2021-12-20 03:33:49 +0000

    UPD: Using useNavigate

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import UsersPage from './pages/UsersPage';
 import NotFoundPage from './pages/NotFoundPage';
 import Navbar from './components/Navbar';
 import UserPage from './pages/UserPage';
+import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Route path='/usuarios' element={<Navigate to='/users' />} />
         <Route path='/usuarios2' element={<Navigate replace to='/users' />} />
         <Route path='/users/:id' element={<UserPage />} />
+        <Route path='/Dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ export default function Navbar() {
             <ul>
                 <li><NavLink className={({ isActive }) => isActive ? 'active' : ''} to='/'>Home</NavLink></li>
                 <li><NavLink to='/about'>About</NavLink></li>
+                <li><NavLink to='/Dashboard'>Dashboard</NavLink></li>
                 <li><NavLink className={({ isActive }) => isActive ? 'active' : 'NotActive'} to='/users'>Users</NavLink></li>
             </ul>
         </div>
