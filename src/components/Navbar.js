fef98da46ccc427360b4a137d973a5f58d0c9f4a
JuanSebastianGB <3393@holbertonschool.com>
2021-12-20 04:01:20 +0000

    STYLE: Refactoring code by implementing semistandard

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import { NavLink } from "react-router-dom";
-import './Navbar.css'
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
-export default function Navbar() {
-    return (
-        <div>
-            <ul>
-                <li><NavLink className={({ isActive }) => isActive ? 'active' : ''} to='/'>Home</NavLink></li>
-                <li><NavLink to='/about'>About</NavLink></li>
-                <li><NavLink to='/Dashboard'>Dashboard</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? 'active' : 'NotActive'} to='/users'>Users</NavLink></li>
-            </ul>
-        </div>
-    )
+export default function Navbar () {
+  return (
+    <div>
+      <ul>
+        <li><NavLink className={({ isActive }) => isActive ? 'active' : ''} to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+        <li><NavLink to='/Dashboard'>Dashboard</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? 'active' : 'NotActive'} to='/users'>Users</NavLink></li>
+      </ul>
+    </div>
+  );
 }
